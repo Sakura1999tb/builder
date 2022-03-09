ec2040d91ba111a27bf46de6a7682e44f328e29d
Toan Le <khanhtoanle38@gmail.com>
2022-03-09 16:31:18 +0700

    st

--- installWindow.sh
+++ installWindow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://gitlab.com/canxb33/app/app_build/builder.git -b 1.0.0 $HOME/.canxb33
+git clone https://gitlab.com/canxb33/app/app_build/builder.git -b v1.0.0/android $HOME/.canxb33
 echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >> $HOME/.bashrc
 echo "[ -s "$CANXB33_DIR/canxb33.sh" ] && \. \"$CANXB33_DIR/canxb33.sh\"" >> $HOME/.bashrc
 . $HOME/.bashrc
