57cbe117ca9c9f409ec80af8b89a97d491ec3e1e
Sakura1999tb <58286539+Sakura1999tb@users.noreply.github.com>
2022-03-09 14:14:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ git clone https://gitlab.com/canxb33/app/app_build/builder.git -b 1.0.0 $HOME/.c
 echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >> $HOME/.bashrc
 echo "[ -s "$CANXB33_DIR/canxb33.sh" ] && \. \"$CANXB33_DIR/canxb33.sh\"" >> $HOME/.bashrc
 . $HOME/.bashrc
-. $HOME/.bash_profile
+# . $HOME/.bash_profile
