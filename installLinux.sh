16e8e75d45420f88e3ac98a2acae886be5fe9f5a
Toan Le <khanhtoanle38@gmail.com>
2022-03-09 14:57:24 +0700

    st

--- installLinux.sh
+++ installLinux.sh
@@ -2,5 +2,5 @@
 git clone https://gitlab.com/canxb33/app/app_build/builder.git -b 1.0.0 $HOME/.canxb33
 echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >>$HOME/.bashrc
 . $HOME/.bashrc
-echo "[ -s \"$CANXB33_DIR/canxb33.sh\" ] && \. \"$CANXB33_DIR/canxb33.sh\"" >>$HOME/.bashrc
+echo "[ -s \"\$CANXB33_DIR/canxb33.sh\" ] && \. \"\$CANXB33_DIR/canxb33.sh\"" >>$HOME/.bashrc
 . $HOME/.bashrc
