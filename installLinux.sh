74c56598b7f1bc6b5d2eb1085f033eb79019623f
Sakura1999tb <58286539+Sakura1999tb@users.noreply.github.com>
2022-03-10 14:47:51 +0700

    Update installLinux.sh

--- installLinux.sh
+++ installLinux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://gitlab.com/canxb33/app/app_build/builder.git -b v1.0.0/android $HOME/.canxb33
+git clone https://gitlab.com/canxb33/app/app_build/builder.git $HOME/.canxb33
 echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >>$HOME/.bashrc
 echo "[ -s \$CANXB33_DIR/canxb33.sh ] && \. \$CANXB33_DIR/canxb33.sh" >>$HOME/.bashrc
