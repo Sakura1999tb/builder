c507510c32d7f217c7ac44331665849dbe884939
Sakura1999tb <58286539+Sakura1999tb@users.noreply.github.com>
2022-03-11 10:57:49 +0700

    Update installMacOS.sh

--- installMacOS.sh
+++ installMacOS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://gitlab.com/canxb33/app/app_build/builder.git $HOME/.canxb33
-echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >>$HOME/.zshrc
-echo "[ -s \$CANXB33_DIR/canxb33.sh ] && \. \$CANXB33_DIR/canxb33.sh" >>$HOME/.zshrc
+echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >>$HOME/.bashrc
+echo "[ -s \$CANXB33_DIR/canxb33.sh ] && \. \$CANXB33_DIR/canxb33.sh" >>$HOME/.bashrc
