c28e41ce774f37378ac5080d35b59b56f1115911
Sakura1999tb <58286539+Sakura1999tb@users.noreply.github.com>
2022-05-24 17:02:39 +0700

    Update installWindow.sh

--- installWindow.sh
+++ installWindow.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git clone https://gitlab.com/canxb33/app/app_build/builder.git --branch 1.0.1 $HOME/.canxb33
+touch ~/.bashrc
+touch ~/.bash_profile
 echo "export CANXB33_DIR=\"$HOME/.canxb33\"" >> $HOME/.bashrc
 echo "[ -s \$CANXB33_DIR/canxb33.sh ] && \. \$CANXB33_DIR/canxb33.sh" >>$HOME/.bashrc
+echo "test -f ~/.profile && . ~/.profile" >> $HOME/.bash_profile
+echo "test -f ~/.bashrc && . ~/.bashrc" >> $HOME/.bash_profile
 . $HOME/.bashrc
 . $HOME/.bash_profile
